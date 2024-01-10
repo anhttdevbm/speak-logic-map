e6a1d6bcac601ae5bb4c2752dfcbeb5c1ad58b87
ntduyen <duyen.nt@tinasoft.vn>
2024-01-11 00:55:15 +0700

    Issue 44, 45.

--- src/components/Tools/RearTools/RearTools.tsx
+++ src/components/Tools/RearTools/RearTools.tsx
@@ -16,25 +16,29 @@ import SwitchOptions from './ToolItems/SwitchOptions';
 import styles from './_RearTools.module.scss';
 
 
-const ItemList: ItemInterface[] = [
-  {value: 'function', Icon: FunctionIcon, name: 'Function', ability: true},
-  {value: 'person', Icon: PersonIcon, name: 'Person', ability: true},
-  {value: 'main-set', Icon: MainsetIcon, name: 'The Given Set', ability: true},
-  {value: 'house', Icon: HouseIcon, name: 'House', ability: true},
-  {value: 'inter-route', Icon: VectorIcon, name: 'Inter-route', ability: true},
-  {value: 'distance', Icon: DistanceIcon, name: 'Distance', ability: true},
-  {value: 'philosophy', Icon: PhilosophyIcon, name: 'Philosophy', ability: false},
-  {value: 'welcome-sign', Icon: WelcomeSignIcon, name: 'Welcome', ability: true},
-  {value: 'scroll-feature', Icon: FunctionPerformanceIcon, name: 'Function Performance', ability: true},
-  {value: 'horizontal-line', Icon: PrincipleLineIcon, name: 'Principle Line', ability: true},
-  {value: 'relate', Icon: RelatedIcon, name: 'Relate', ability: true},
-  {value: 'mobility', Icon: MobilityIcon, name: 'Mobility', ability: true},
-
-]
-
 const RearTools: React.FC = (): JSX.Element => {
   const [isExpanded, setIsExpanded] = useState<boolean>(true);
   const globalStore = useGlobalStore();
+
+  let isSelectedMoreViewFunction = globalStore.moreName === 'world-as-function';
+  let isSelectedMoreViewPerson = globalStore.moreName === 'population-view' || globalStore.moreName === 'population-view-with-country' || globalStore.moreName === 'population-view-principle-line';
+  let ability = isSelectedMoreViewPerson || isSelectedMoreViewFunction;
+
+  const ItemList: ItemInterface[] = [
+    {value: 'function', Icon: FunctionIcon, name: 'Function', ability: !isSelectedMoreViewPerson},
+    {value: 'person', Icon: PersonIcon, name: 'Person', ability: !isSelectedMoreViewFunction},
+    {value: 'main-set', Icon: MainsetIcon, name: 'The Given Set', ability: !ability},
+    {value: 'house', Icon: HouseIcon, name: 'House', ability: !ability},
+    {value: 'inter-route', Icon: VectorIcon, name: 'Inter-route', ability: !ability},
+    {value: 'distance', Icon: DistanceIcon, name: 'Distance', ability: !ability},
+    {value: 'philosophy', Icon: PhilosophyIcon, name: 'Philosophy', ability: false},
+    {value: 'welcome-sign', Icon: WelcomeSignIcon, name: 'Welcome', ability: !ability},
+    {value: 'scroll-feature', Icon: FunctionPerformanceIcon, name: 'Function Performance', ability: !ability},
+    {value: 'horizontal-line', Icon: PrincipleLineIcon, name: 'Principle Line', ability: !ability},
+    {value: 'relate', Icon: RelatedIcon, name: 'Relate', ability: !ability},
+    {value: 'mobility', Icon: MobilityIcon, name: 'Mobility', ability: !ability},
+  ]
+
   return (
     <div className={`${styles['rear-tools-wrap']} ${isExpanded ? styles['expanded'] : styles['minimal']}`}>
       <div className={`${styles['rear-tools-content']}`}>
