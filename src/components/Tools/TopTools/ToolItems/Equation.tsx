b51efc4d7fd4539af86d396a95b0f57d5269d95c
hanhnx <xuanhanhbn2@gmail.com>
2025-04-29 21:56:48 +0700

    update code

--- src/components/Tools/TopTools/ToolItems/Equation.tsx
+++ src/components/Tools/TopTools/ToolItems/Equation.tsx
@@ -5,11 +5,12 @@ import React, { useCallback, useEffect, useState } from "react";
 import { useGlobalStore, useSimulationSettingStore } from "@/providers/RootStoreProvider";
 import styles from "./_ToolItem.module.scss";
 import { RelatedIcon } from "@/components/Icons/Icons";
-import { Input, Modal, Select } from "antd";
+import { Button, Collapse, CollapseProps, Input, Modal, Select } from "antd";
 import { OPTIONS_EQUATION, OPTIONS_MATHEMATICAL } from "./constants";
 
 import Head from "next/head";
 
+const { TextArea } = Input;
 const MathJaxScript = () => (
   <Head>
     <script type="text/javascript" id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
@@ -80,8 +81,24 @@ const Equation = () => {
     return label;
   };
 
+  const getItemsCollapse = () => {
+    return OPTIONS_EQUATION.map((obj) => ({
+      key: obj.value,
+      label: obj.label,
+      children: (
+        <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
+          {OPTIONS_MATHEMATICAL.filter((option) => option.parentId === obj.value).map((options) => (
+            <Button className={`${styles["center-items"]}`} key={options.value} onClick={() => handleUpdateDataRequest(options.value, "equationType")}>
+              <span dangerouslySetInnerHTML={{ __html: renderMath(options.label) }} />
+            </Button>
+          ))}
+        </div>
+      ),
+    }));
+  };
+
   return (
-    <div>
+    <div id="modal-add-equation">
       <MathJaxScript />
       <button type="button" className={`${styles["left-item-wrap"]} ${globalStore.simulation ? styles["active"] : null}`} onClick={() => setIsOpenModal(true)}>
         <RelatedIcon />
@@ -97,20 +114,20 @@ const Equation = () => {
           title="Select Equation"
         >
           <div>
-            {/* <div className={`${styles["center-items"]}`}>
-              <div style={{ width: "50%" }}>Select Equation</div>
-              <div className={`${styles["end-items"]}`} style={{ flex: 1 }}>
-                <Select
-                  placeholder="Select Equation"
-                  style={{ width: "100%" }}
-                  size="large"
-                  options={OPTIONS_EQUATION}
-                  value={dataRequest.equation}
-                  onChange={(value) => handleUpdateDataRequest(value, "equation")}
-                />
-              </div>
-            </div> */}
-            {/* {dataRequest.equation && dataRequest.equation !== "customize" && ( */}
+            <Collapse items={getItemsCollapse()} defaultActiveKey={["function"]} />
+            <div style={{ marginTop: "20px", marginBottom: "20px" }}>
+              <div style={{ width: "100%" }}>Value</div>
+              <TextArea
+                placeholder="Input Value"
+                style={{ width: "100%" }}
+                showCount
+                size="large"
+                value={dataRequest.equationValue || ""}
+                onChange={(e) => handleUpdateDataRequest(e.target.value, "equationValue")}
+              />
+            </div>
+          </div>
+          {/* <div>
             <div className={`${styles["center-items"]}`} style={{ marginTop: "20px" }}>
               <div style={{ width: "100%" }}>Select Mathematical</div>
               <Select
@@ -125,8 +142,6 @@ const Equation = () => {
                 }))}
               />
             </div>
-            {/* )} */}
-
             <div className={`${styles["center-items"]}`} style={{ marginTop: "20px" }}>
               <div style={{ width: "100%" }}>Input Value</div>
               <Input
@@ -137,7 +152,7 @@ const Equation = () => {
                 onChange={(e) => handleUpdateDataRequest(e.target.value, "equationValue")}
               />
             </div>
-          </div>
+          </div> */}
         </Modal>
       )}
     </div>
