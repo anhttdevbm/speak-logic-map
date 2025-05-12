f7426a0c23715bca2a7b5cea7db78c925a61b42b
hanhnx <xuanhanhbn2@gmail.com>
2025-05-12 22:00:39 +0700

    add image

--- src/components/Tools/TopTools/ToolItems/constants.ts
+++ src/components/Tools/TopTools/ToolItems/constants.ts
@@ -1,9 +1,71 @@
 import Image1 from "@/assets/images/Integral-07.png";
-import Image2 from "@/assets/images/function/x-hat.png";
 import Image3 from "@/assets/images/Fraction/1.png";
 import Image4 from "@/assets/images/Theory/1.png";
-import Image5 from "@/assets/images/Theory/2.png";
+// import Image5 from "@/assets/images/Theory/2.png";
 import Image6 from "@/assets/images/Fraction/2.png";
+
+import Image5 from "@/assets/images/Application/Application_1.svg";
+import Image7 from "@/assets/images/Application/Application_2.svg";
+import Image8 from "@/assets/images/Application/Application_3.svg";
+import Image9 from "@/assets/images/Application/Application_4.svg";
+import Image10 from "@/assets/images/Application/Application_5.svg";
+import Image11 from "@/assets/images/Application/Application_6.svg";
+
+import Image12 from "@/assets/images/function/function_1.svg";
+import Image13 from "@/assets/images/function/function_2.svg";
+import Image14 from "@/assets/images/function/function_3.svg";
+import Image15 from "@/assets/images/function/function_4.svg";
+import Image16 from "@/assets/images/function/function_5.svg";
+import Image17 from "@/assets/images/function/function_6.svg";
+import Image18 from "@/assets/images/function/function_7.svg";
+import Image19 from "@/assets/images/function/function_8.svg";
+import Image20 from "@/assets/images/function/function_9.svg";
+import Image21 from "@/assets/images/function/function_10.svg";
+import Image22 from "@/assets/images/function/function_11.svg";
+import Image23 from "@/assets/images/function/function_12.svg";
+import Image24 from "@/assets/images/function/function_13.svg";
+import Image25 from "@/assets/images/function/function_14.svg";
+
+import Image26 from "@/assets/images/Words/word_1.svg";
+import Image27 from "@/assets/images/Words/word_2.svg";
+import Image28 from "@/assets/images/Words/word_3.svg";
+import Image29 from "@/assets/images/Words/word_4.svg";
+import Image30 from "@/assets/images/Words/word_5.svg";
+import Image31 from "@/assets/images/Words/word_6.svg";
+import Image32 from "@/assets/images/Words/word_7.svg";
+import Image33 from "@/assets/images/Words/word_8.svg";
+import Image34 from "@/assets/images/Words/word_9.svg";
+import Image35 from "@/assets/images/Words/word_10.svg";
+import Image36 from "@/assets/images/Words/word_13.svg";
+import Image37 from "@/assets/images/Words/word_12.svg";
+
+import Image38 from "@/assets/images/Fundamental/Theory_1.svg";
+import Image39 from "@/assets/images/Fundamental/Theory_2.svg";
+import Image40 from "@/assets/images/Fundamental/Theory_3.svg";
+import Image41 from "@/assets/images/Fundamental/Theory_4.svg";
+import Image42 from "@/assets/images/Fundamental/Theory_5.svg";
+import Image43 from "@/assets/images/Fundamental/Theory_6.svg";
+import Image44 from "@/assets/images/Fundamental/Theory_7.svg";
+import Image45 from "@/assets/images/Fundamental/Theory_8.svg";
+import Image46 from "@/assets/images/Fundamental/Theory_9.svg";
+import Image47 from "@/assets/images/Fundamental/Theory_10.svg";
+import Image48 from "@/assets/images/Fundamental/Theory_11.svg";
+
+import Image49 from "@/assets/images/Theory/Theory_Dropdown_1.svg";
+import Image50 from "@/assets/images/Theory/Theory_Dropdown_2.svg";
+import Image51 from "@/assets/images/Theory/Theory_Dropdown_3.svg";
+import Image52 from "@/assets/images/Theory/Theory_Dropdown_4.svg";
+import Image53 from "@/assets/images/Theory/Theory_Dropdown_5.svg";
+import Image54 from "@/assets/images/Theory/Theory_Dropdown_6.svg";
+import Image55 from "@/assets/images/Theory/Theory_Dropdown_7.svg";
+import Image56 from "@/assets/images/Theory/Theory_Dropdown_8.svg";
+import Image57 from "@/assets/images/Theory/Theory_Dropdown_9.svg";
+import Image58 from "@/assets/images/Theory/Theory_Dropdown_10.svg";
+import Image59 from "@/assets/images/Theory/Theory_Dropdown_11.svg";
+import Image60 from "@/assets/images/Theory/Theory_Dropdown_12.svg";
+import Image61 from "@/assets/images/Theory/Theory_Dropdown_13.svg";
+import Image62 from "@/assets/images/Theory/Theory_Dropdown_14.svg";
+import Image63 from "@/assets/images/Theory/Theory_Dropdown_16.svg";
 
 export const OPTIONS_EQUATION = [
   {
@@ -37,73 +99,73 @@ export const OPTIONS_EQUATION = [
 ];
 
 export const OPTIONS_MATHEMATICAL = [
-  // FUNCTION-related labels
-  { value: "1", label: "x", parentId: "function" },
-  { value: "2", label: "f_0(x)", parentId: "function" },
-  { value: "3", label: "\\overline{x}", parentId: "function" },
-  { value: "4", label: "f(x)", parentId: "function" },
-  { value: "5", label: "f_i(x)", parentId: "function" },
-  { value: "6", label: "P_i", parentId: "function" },
-  { value: "7", label: "G_i", parentId: "function" },
-  { value: "8", label: "G_i(x)", parentId: "function" },
-  { value: "9", label: "f(x)", parentId: "function" },
-  { value: "10", label: "ECF(x)", parentId: "function" },
-  { value: "11", label: "ECF_i", parentId: "function" },
-  { value: "12", label: "f_i(\\overline{x}_i)", parentId: "function" },
-  { value: "13", label: "x_i", parentId: "function" },
-  { value: "14", label: "f_i(x)", parentId: "function", image: Image1 },
+  // Theory-related labels
+  { value: "33", label: "T_1", parentId: "theory", image: Image49 },
+  { value: "34", label: "T_2", parentId: "theory", image: Image50 },
+  { value: "35", label: "T_3", parentId: "theory", image: Image51 },
+  { value: "36", label: "T_4", parentId: "theory", image: Image52 },
+  { value: "37", label: "T_5", parentId: "theory", image: Image53 },
+  { value: "38", label: "T_6", parentId: "theory", image: Image54 },
+  { value: "39", label: "T_7", parentId: "theory", image: Image55 },
+  { value: "40", label: "T_8", parentId: "theory", image: Image56 },
+  { value: "41", label: "T_9", parentId: "theory", image: Image57 },
+  { value: "42", label: "T_{10}", parentId: "theory", image: Image58 },
+  { value: "43", label: "T", parentId: "theory", image: Image59 },
+  { value: "44", label: "T_0", parentId: "theory", image: Image60 },
+  { value: "45", label: "A", parentId: "theory", image: Image61 },
+  { value: "46", label: "A'", parentId: "theory", image: Image62 },
+  { value: "47", label: "A'_0", parentId: "theory", image: Image63 },
+
+  // Fundamental-related labels
+  { value: "48", label: "K_T", parentId: "fundamental", image: Image38 },
+  { value: "49", label: "i_T", parentId: "fundamental", image: Image39 },
+  { value: "50", label: "I_T", parentId: "fundamental", image: Image40 },
+  { value: "51", label: "E_T", parentId: "fundamental", image: Image41 },
+  { value: "52", label: "P_T", parentId: "fundamental", image: Image42 },
+  { value: "53", label: "M_T", parentId: "fundamental", image: Image43 },
+  { value: "54", label: "Es_T", parentId: "fundamental", image: Image44 },
+  { value: "55", label: "W_T", parentId: "fundamental", image: Image45 },
+  { value: "56", label: "G_T", parentId: "fundamental", image: Image46 },
+  { value: "57", label: "X_T", parentId: "fundamental", image: Image47 },
+  { value: "58", label: "U_T", parentId: "fundamental", image: Image48 },
 
   // WORD-related labels
-  { value: "15", label: "W", parentId: "word" },
-  { value: "16", label: "W_0", parentId: "word" },
-  { value: "17", label: "\\widehat{W}", parentId: "word" },
-  { value: "18", label: "\\widehat{W}_0", parentId: "word" },
-  { value: "19", label: "\\overline{W}", parentId: "word" },
-  { value: "20", label: "S(W)", parentId: "word" },
-  { value: "21", label: "S_i(W)", parentId: "word" },
-  { value: "22", label: "G_i(W)", parentId: "word" },
-  { value: "23", label: "D(W)", parentId: "word" },
-  { value: "24", label: "D(\\overline{W})", parentId: "word" },
-  { value: "25", label: "G(\\overline{W})", parentId: "word" },
-  { value: "26", label: "S(\\overline{W})", parentId: "word" },
+  { value: "15", label: "W", parentId: "word", image: Image26 },
+  { value: "16", label: "W_0", parentId: "word", image: Image27 },
+  { value: "17", label: "\\widehat{W}", parentId: "word", image: Image28 },
+  { value: "18", label: "\\widehat{W}_0", parentId: "word", image: Image29 },
+  { value: "19", label: "\\overline{W}", parentId: "word", image: Image30 },
+  { value: "20", label: "S(W)", parentId: "word", image: Image31 },
+  { value: "21", label: "S_i(W)", parentId: "word", image: Image32 },
+  { value: "22", label: "G_i(W)", parentId: "word", image: Image33 },
+  { value: "23", label: "D(W)", parentId: "word", image: Image34 },
+  { value: "24", label: "D(\\overline{W})", parentId: "word", image: Image35 },
+  { value: "25", label: "G(\\overline{W})", parentId: "word", image: Image36 },
+  { value: "26", label: "S(\\overline{W})", parentId: "word", image: Image37 },
+
+  // FUNCTION-related labels
+  { value: "1", label: "x", parentId: "function", image: Image12 },
+  { value: "2", label: "f_0(x)", parentId: "function", image: Image13 },
+  { value: "3", label: "\\overline{x}", parentId: "function", image: Image14 },
+  { value: "4", label: "f(x)", parentId: "function", image: Image15 },
+  { value: "5", label: "f_i(x)", parentId: "function", image: Image16 },
+  { value: "6", label: "P_i", parentId: "function", image: Image17 },
+  { value: "7", label: "G_i", parentId: "function", image: Image18 },
+  { value: "8", label: "G_i(x)", parentId: "function", image: Image19 },
+  { value: "9", label: "f(x)", parentId: "function", image: Image20 },
+  { value: "10", label: "ECF(x)", parentId: "function", image: Image21 },
+  { value: "11", label: "ECF_i", parentId: "function", image: Image22 },
+  { value: "12", label: "f_i(\\overline{x}_i)", parentId: "function", image: Image23 },
+  { value: "13", label: "x_i", parentId: "function", image: Image24 },
+  { value: "14", label: "f_i(x)", parentId: "function", image: Image25 },
 
   // Application-related labels
   { value: "27", label: "A", parentId: "application", image: Image5 },
-  { value: "28", label: "A_0", parentId: "application" },
-  { value: "29", label: "\\widehat{A}", parentId: "application" },
-  { value: "30", label: "\\overline{A}", parentId: "application" },
-  { value: "31", label: "\\widehat{A}_0", parentId: "application" },
-  { value: "32", label: "\\overline{A}_0", parentId: "application" },
-
-  // Theory-related labels
-  { value: "33", label: "T_1", parentId: "theory" },
-  { value: "34", label: "T_2", parentId: "theory" },
-  { value: "35", label: "T_3", parentId: "theory" },
-  { value: "36", label: "T_4", parentId: "theory" },
-  { value: "37", label: "T_5", parentId: "theory" },
-  { value: "38", label: "T_6", parentId: "theory" },
-  { value: "39", label: "T_7", parentId: "theory" },
-  { value: "40", label: "T_8", parentId: "theory" },
-  { value: "41", label: "T_9", parentId: "theory" },
-  { value: "42", label: "T_{10}", parentId: "theory" },
-  { value: "43", label: "T", parentId: "theory", image: Image4 },
-  { value: "44", label: "T_0", parentId: "theory" },
-  { value: "45", label: "A", parentId: "theory", image: Image5 },
-  { value: "46", label: "A'", parentId: "theory" },
-  { value: "47", label: "A'_0", parentId: "theory" },
-
-  // Fundamental-related labels
-  { value: "48", label: "K_T", parentId: "fundamental" },
-  { value: "49", label: "i_T", parentId: "fundamental" },
-  { value: "50", label: "I_T", parentId: "fundamental" },
-  { value: "51", label: "E_T", parentId: "fundamental" },
-  { value: "52", label: "P_T", parentId: "fundamental" },
-  { value: "53", label: "M_T", parentId: "fundamental" },
-  { value: "54", label: "Es_T", parentId: "fundamental" },
-  { value: "55", label: "W_T", parentId: "fundamental" },
-  { value: "56", label: "G_T", parentId: "fundamental" },
-  { value: "57", label: "X_T", parentId: "fundamental" },
-  { value: "58", label: "U_T", parentId: "fundamental" },
+  { value: "28", label: "A_0", parentId: "application", image: Image7 },
+  { value: "29", label: "\\widehat{A}", parentId: "application", image: Image8 },
+  { value: "30", label: "\\overline{A}", parentId: "application", image: Image9 },
+  { value: "31", label: "\\widehat{A}_0", parentId: "application", image: Image10 },
+  { value: "32", label: "\\overline{A}_0", parentId: "application", image: Image11 },
 
   // FRACTION & RADICAL
   { value: "59", label: "\\sqrt{x}", parentId: "fraction" },
